a3f18e96bbbd07aee7f5edfbbd931a6c5953f6b6
Mikhail <sovremen1@gmail.com>
2020-04-03 23:04:48 +0300

    Мультиплеер

---
+++ Assets/source/ClientConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+namespace Source
+{
+	public class ClientConnection
+	{
+		public TcpClient client;
+		public NetworkStream stream;
+		public StreamReader reader;
+		public StreamWriter writer;
+		public string nickname;
+		public Vector3 position;
+		public Quaternion rotation;
+		public bool spawned = false;
+		public GameObject vehicle;
+		public int id;
+		public Thread networkThread;
+	}
+}
